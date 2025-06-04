2d2e80b6490330efae81d84f588ef733ff997731
Zeel Dhengre <136904394+Zeel-13@users.noreply.github.com>
2025-06-04 16:29:30 +0530

    Update settings.py

--- dental/settings.py
+++ dental/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'zeel.test.mail@gmail.com'
-EMAIL_HOST_PASSWORD = 'wdsa cwed scuk zuoa'
+EMAIL_HOST_USER = 'xyz@gmail.com'   # enter your email id
+EMAIL_HOST_PASSWORD = 'abcd efgh ijkl mnop' # enter your email host password
